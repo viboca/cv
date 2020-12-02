542a68b50cef8e7778b565cd01fd20da97c71770
Accountant77 <vbonilla.77@gmail.com>
2020-12-03 00:25:37 +0100

    PMV - presentacion

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,4 +1,9 @@
 Swal.fire({
     title: 'Bienvenido!',
-    text: 'Este sitio web sigue en proceso de desarrollo, diviertete!'
+    text: 'Este sitio web sigue en proceso de desarrollo, diviertete!',
+    icon: 'info',
+    width: '90%',
+    padding: '1rem',
+    timer: '6000',
+    timeProgressBar: 'true',
 })
